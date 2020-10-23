0229eea17cadc31ea9f319102263aa0a6eaf8938
Richard Knoll <riknoll@users.noreply.github.com>
2020-10-23 13:38:21 -0700

    Add small tilemaps

--- main.ts
+++ main.ts
@@ -1,1 +1,15 @@
 
+namespace tiles {
+    //% blockId=small_tilemap_editor block="set 8x8 tilemap to $tilemap"
+    //% weight=200 blockGap=8
+    //% tilemap.fieldEditor="tilemap"
+    //% tilemap.fieldOptions.decompileArgumentAsString="true"
+    //% tilemap.fieldOptions.filter="tile"
+    //% tilemap.fieldOptions.taggedTemplate="tilemap"
+    //% tilemap.fieldOptions.tileWidth=8
+    //% blockNamespace="scene" group="Tiles" duplicateShadowOnDrag
+    //% help=tiles/set-tile-map
+    export function setSmallTilemap(tilemap: TileMapData) {
+        scene.setTileMapLevel(tilemap);
+    }
+}
